d79261b917313429428f7edae1d6d3f72957762a
Timothy Guan <guan.chuan.siong@gmail.com>
2026-05-31 20:07:17 +1000

    feat: add swagger UI support

--- OrderManagement.API/Program.cs
+++ OrderManagement.API/Program.cs
@@ -19,9 +19,9 @@ builder.Services.AddScoped<OrderService>();
 // Add controller support
 builder.Services.AddControllers();
 
-// Register OpenAPI generation service
-// Learn more at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Register Swagger generation service
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 #endregion
 
 #region middleware
@@ -30,7 +30,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
