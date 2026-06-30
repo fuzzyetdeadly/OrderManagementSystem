386361255e6344a0b92f42c7da0efc824c78bea3
Timothy Guan <guan.chuan.siong@gmail.com>
2026-06-30 12:49:32 +1000

    fix: setup now correctly cleans up listeners after each test

--- Web.React/src/setupTests.ts
+++ Web.React/src/setupTests.ts
@@ -2,6 +2,8 @@ import { server } from "./mocks/server";
 
 // For every test file, start the MSW server, reset between tests, and close after
 beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.events.removeAllListeners()); /* prevent listener bleed */
+afterEach(() => {
+  server.resetHandlers();
+  server.events.removeAllListeners();
+});
 afterAll(() => server.close());
