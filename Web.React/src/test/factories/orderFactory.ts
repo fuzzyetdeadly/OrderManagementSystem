6bda2fda08e04d7761c2aae6c55728a6286bc987
Timothy Guan <guan.chuan.siong@gmail.com>
2026-07-01 19:07:37 +1000

    chore: convert order factory to function (for traceability)

--- Web.React/src/test/factories/orderFactory.ts
+++ Web.React/src/test/factories/orderFactory.ts
@@ -1,11 +1,15 @@
 import type { Order } from "../../types/order";
 
 // Note: Partial<Order> allows partial overrides of properties
-export const makeOrder = (overrides: Partial<Order> = {}): Order => ({
-  id: 1,
-  status: "Pending",
-  created: "2023-01-01T12:00:00Z" /* don't care */,
-  customerId: 1,
-  items: [{ productName: "Potato", quantity: 1, unitPrice: 0.99 }],
-  ...overrides,
-});
+// 'id' collisions should be overridden as necessary in tests
+// rolling id negatively impacts test maintainability
+export function makeOrder(overrides: Partial<Order> = {}): Order {
+  return {
+    id: 1,
+    status: "Pending",
+    created: "2023-01-01T12:00:00Z" /* don't care */,
+    customerId: 1,
+    items: [{ productName: "Potato", quantity: 1, unitPrice: 0.99 }],
+    ...overrides,
+  };
+}
