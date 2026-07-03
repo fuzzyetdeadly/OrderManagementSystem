e3c30959998b0c0b457e767f9d57f90cfb80cdf9
Timothy Guan <guan.chuan.siong@gmail.com>
2026-07-03 19:05:34 +1000

    chore: update App to use translations

--- Web.React/src/test/setup.ts
+++ Web.React/src/test/setup.ts
@@ -1,5 +1,10 @@
-import "@testing-library/jest-dom/vitest"; /* executes modules code for side-effects */
+import "@testing-library/jest-dom/vitest"; // executes modules code for side-effects
 import { server } from "./mocks/server";
+
+// Intercept module and override 'useTranslation' 't' function to return keys as value
+vi.mock("react-i18next", () => ({
+  useTranslation: () => ({ t: (key: string) => key }),
+}));
 
 // For every test file, start the MSW server, reset between tests, and close after
 beforeAll(() => server.listen());
