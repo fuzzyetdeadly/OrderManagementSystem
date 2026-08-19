87d714f2ea76162f137e59e4348b3dbb69ed2150
Timothy Guan <guan.chuan.siong@gmail.com>
2026-08-19 17:29:55 +1000

    test: add classes for order controller patch/delete

---
+++ OrderManagement.Tests/API/OrderControllerDeleteTests.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderManagement.Tests.API
+{
+    public class OrderControllerDeleteTests : OrderControllerTestsBase
+    {
+        public OrderControllerDeleteTests(CustomWebApplicationFactory factory, ITestOutputHelper output)
+        : base(factory, output) { }
+
+        #region Delete
+
+        #endregion
+    }
+}
---
+++ OrderManagement.Tests/API/OrderControllerPatchTests.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderManagement.Tests.API
+{
+    public class OrderControllerPatchTests : OrderControllerTestsBase
+    {
+        public OrderControllerPatchTests(CustomWebApplicationFactory factory, ITestOutputHelper output)
+        : base(factory, output) { }
+
+        #region UpdateStatus
+
+        #endregion
+    }
+}
